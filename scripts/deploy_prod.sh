57f8163a377caec2bb9ef12727a5a8eb16d42566
Ethan Lane <ethan@vylpes.com>
2023-09-18 10:30:01 +0100

    Add stage deploy script

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -7,7 +7,7 @@ cd ~/apps/droplet/droplet_prod \
 && git checkout main \
 && git fetch \
 && git pull \
-&& docker compose --file docker-compose.prod.yml down \
+&& docker compose down \
 && (pm2 stop droplet_prod || true) \
 && (pm2 delete droplet_prod || true) \
 && node ./scripts/generate_secret > secret.txt \
@@ -15,7 +15,7 @@ cd ~/apps/droplet/droplet_prod \
 && yarn clean \
 && yarn install --frozen-lockfile \
 && yarn build \
-&& docker compose --file docker-compose.prod.yml up -d \
+&& docker compose up -d \
 && echo "Sleeping for 10 seconds to let database load..." \
 && sleep 10 \
 && yarn run db:up \
