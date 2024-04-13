662b2ccc6689aef671479993b411c121067b0295
Ethan Lane <ethan@vylpes.com>
2024-04-13 17:12:47 +0100

    Create logout tests

--- tests/routes/auth/logout.test.ts
+++ tests/routes/auth/logout.test.ts
@@ -1,5 +1,31 @@
-describe("OnGet", () => {
-    test.todo("EXPECT router to be defined");
+import Logout from "../../../src/routes/auth/logout";
+import { Request, Response } from "express";
 
-    test.todo("EXPECT logout to be successful");
+describe("OnGet", () => {
+    test("EXPECT logout to be successful", () => {
+        // Arrange
+        const req = {
+            session: {
+                destroy: jest.fn().mockImplementation((callback: Function) => {
+                    callback();
+                }),
+            }
+        } as unknown as Request;
+
+        const res = {
+            redirect: jest.fn(),
+        } as unknown as Response;
+
+        const next = jest.fn();
+
+        // Act
+        const route = new Logout();
+        route.OnGet(req, res, next);
+
+        // Assert
+        expect(req.session.destroy).toHaveBeenCalledTimes(1);
+
+        expect(res.redirect).toHaveBeenCalledTimes(1);
+        expect(res.redirect).toHaveBeenCalledWith("/");
+    });
 });
