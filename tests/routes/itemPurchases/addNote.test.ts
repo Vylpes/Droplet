19662f4bc48e162c74e7e822475c595b51459464
Ethan Lane <ethan@vylpes.com>
2024-06-01 13:42:31 +0100

    Create addnote item purchase tests

--- tests/routes/itemPurchases/addNote.test.ts
+++ tests/routes/itemPurchases/addNote.test.ts
@@ -53,5 +53,42 @@ describe("OnPostAsync", () => {
         expect(savedNote!.ForId).toBe("purchaseId");
     });
 
-    test.todo("GIVEN body is invalid, EXPECT redirect to itemPurchase page");
+    test("GIVEN body is invalid, EXPECT redirect to itemPurchase page", async () => {
+        let savedNote: Note | undefined;
+
+        // Arrange
+        const req = {
+            params: {
+                Id: "purchaseId",
+            },
+            body: {
+                text: "note text",
+            }
+        } as unknown as Request;
+
+        const res = {
+            redirect: jest.fn(),
+        } as unknown as Response;
+
+        BodyValidator.prototype.NotEmpty = jest.fn().mockReturnThis();
+        BodyValidator.prototype.Validate = jest.fn().mockResolvedValue(false);
+
+        Note.prototype.Save = jest.fn().mockImplementation((_, note: Note) => {
+            savedNote = note;
+        });
+
+        // Act
+        const page = new AddNote();
+        await page.OnPostAsync(req, res);
+
+        // Assert
+        expect(res.redirect).toHaveBeenCalledTimes(1);
+        expect(res.redirect).toHaveBeenCalledWith("/item-purchases/view/purchaseId");
+
+        expect(BodyValidator.prototype.Validate).toHaveBeenCalledTimes(1);
+
+        expect(savedNote).toBeUndefined();
+
+        expect(Note.prototype.Save).not.toHaveBeenCalled();
+    });
 });
